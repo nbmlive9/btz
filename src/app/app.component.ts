b7311516401eef492c01d964228cda45b4b8fc70
PavanKumar123230 <reddaboinapavan49@gmail.com>
2025-08-26 14:58:11 +0530

    loginand signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bitraze';
+  constructor(public router: Router) {}
+
+  // Helper method to check if current route is login or signup
+  hideBottomNav(): boolean {
+    return this.router.url === '/login' || this.router.url === '/sign';
+  }
+
+
 }
